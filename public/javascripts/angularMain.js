2c4a9f87f82e2425b8539a1cc099f46c3cfefb9d
Yusuf Osman <yusuf.o@gmail.com>
2016-03-03 23:47:01 +0000

    Directive expand div works

--- public/javascripts/angularMain.js
+++ public/javascripts/angularMain.js
@@ -11,16 +11,16 @@ app.controller('MainCtrl', [
         $scope.addPost = function(){
             $scope.posts.push({title: 'This is one great article', subtitle: 'Aren\'t subtitles great?', submitter: 'Yusuf Osman', date: Date(), image: 'http://www.architectsjournal.co.uk/pictures/2000x2000fit/9/4/2/1369942_London_Skyline.jpg', upvotes: 0});
         };
-
-        $scope.createPost = function($event){
-            document.getElementById()
-        }
     }]);
 
 app.directive('writePostBox', function() {
     return {
         link: function(scope, element, attrs){
-            element.css('height', '200px');
+            var expandWriteBox = function() {
+                element.css('height', '200px');
+            };
+            element.bind('click', expandWriteBox);
+            //element.css('height', '200px');
             //element.height($window.innerHeight/3);
         }
     };
