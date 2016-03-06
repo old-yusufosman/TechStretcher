55f3dca46d1c6504a5923f0e4176827bac218fb2
Yusuf Osman <yusuf.o@gmail.com>
2016-03-06 22:07:42 +0000

    Controller alterations

--- public/javascripts/angularMain.js
+++ public/javascripts/angularMain.js
@@ -1,6 +1,6 @@
 var app = angular.module('TechStretcher', [])
 
-app.controller('MainCtrl', [
+app.controller('MainController', [
     '$scope',
     function($scope){
         $scope.fullname = 'yusuf osman';
@@ -8,9 +8,6 @@ app.controller('MainCtrl', [
             {title: 'This is one great article', subtitle: 'Aren\'t subtitles great?', submitter: 'Yusuf Osman', date: '2 days ago', image: 'http://www.architectsjournal.co.uk/pictures/2000x2000fit/9/4/2/1369942_London_Skyline.jpg', upvotes: 9},
             {title: 'There should be more articles like this...', subtitle: 'Yes I absolutely agree', submitter: 'Yusuf Osman', date: '5 hours ago', image: 'https://wallpaperscraft.com/image/thailand_sea_ocean_boat_rock_95477_1920x1080.jpg',  upvotes: 3},
         ];
-        $scope.addPost = function(){
-            $scope.posts.push({title: 'This is one great article', subtitle: 'Aren\'t subtitles great?', submitter: 'Yusuf Osman', date: Date(), image: 'http://www.architectsjournal.co.uk/pictures/2000x2000fit/9/4/2/1369942_London_Skyline.jpg', upvotes: 0});
-        };
     }]);
 
 app.directive('writePostBox', function() {
