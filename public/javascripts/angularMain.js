3a7d766c6d5ca42b2bf5cc37e624049fd849e1cb
Yusuf Osman <yusuf.o@gmail.com>
2016-03-08 16:46:18 +0000

    Simple posting works

--- public/javascripts/angularMain.js
+++ public/javascripts/angularMain.js
@@ -6,9 +6,14 @@ app.controller('MainController', [
         $scope.fullname = 'Yusuf Osman';
         $scope.showContents = false;
         $scope.posts = [
-            {title: 'This is one great article', subtitle: 'Aren\'t subtitles great?', submitter: 'Yusuf Osman', date: '2 days ago', image: 'http://www.architectsjournal.co.uk/pictures/2000x2000fit/9/4/2/1369942_London_Skyline.jpg', content: "", upvotes: 9},
-            {title: 'There should be more articles like this...', subtitle: 'Yes I absolutely agree', submitter: 'Yusuf Osman', date: '5 hours ago', image: 'https://wallpaperscraft.com/image/thailand_sea_ocean_boat_rock_95477_1920x1080.jpg',  content: "", upvotes: 3},
+            {title: 'This is one great article', subtitle: 'Aren\'t subtitles great?', submitter: 'Yusuf Osman', date: '2 days ago', image: 'http://www.architectsjournal.co.uk/pictures/2000x2000fit/9/4/2/1369942_London_Skyline.jpg', postcontent: "", upvotes: 9},
+            {title: 'There should be more articles like this...', subtitle: 'Yes I absolutely agree', submitter: 'Yusuf Osman', date: '5 hours ago', image: 'https://wallpaperscraft.com/image/thailand_sea_ocean_boat_rock_95477_1920x1080.jpg',  postcontent: "", upvotes: 3},
         ];
+        $scope.addPost = function(){
+            if(!$scope.postcontent || $scope.postcontent === '') { return; }
+            $scope.posts.push({submitter: 'Yusuf Osman', date: Date(), postcontent: $scope.postcontent, upvotes: 0});
+            $scope.postcontent = "";
+        };
     }]);
 
 app.directive('writePostBox', function() {
