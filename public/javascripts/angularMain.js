1be71405080df8bc914d0dcadc9719ab825b744f
Yusuf Osman <yusuf.o@gmail.com>
2016-03-08 21:52:41 +0000

    posts factory created

--- public/javascripts/angularMain.js
+++ public/javascripts/angularMain.js
@@ -1,9 +1,17 @@
-var app = angular.module('TechStretcher', []);
+var app = angular.module('TechStretcher', ['ui.router']);
 app.constant('moment', moment)
+
+app.factory('posts', [function(){
+    var o = {
+        posts: []
+    };
+    return o;
+}]);
 
 app.controller('MainController', [
     '$scope',
-    function($scope){
+    'posts',
+    function($scope, posts){
         $scope.fullname = 'Yusuf Osman';
         $scope.showContents = false;
 
@@ -13,10 +21,13 @@ app.controller('MainController', [
         var d2 = new Date();
         d2.setDate(d2.getDate() - 3);
 
-        $scope.posts = [
-            {title: 'This is one great article', subtitle: 'Aren\'t subtitles great?', submitter: 'Yusuf Osman', date: d, image: 'http://www.architectsjournal.co.uk/pictures/2000x2000fit/9/4/2/1369942_London_Skyline.jpg', postcontent: "", upvotes: 9},
-            {title: 'There should be more articles like this...', subtitle: 'Yes I absolutely agree', submitter: 'Yusuf Osman', date: d2, image: 'https://wallpaperscraft.com/image/thailand_sea_ocean_boat_rock_95477_1920x1080.jpg',  postcontent: "", upvotes: 3},
-        ];
+        $scope.posts = posts.posts;
+
+        //$scope.posts = [
+        //    {title: 'This is one great article', subtitle: 'Aren\'t subtitles great?', submitter: 'Yusuf Osman', date: d, image: 'http://www.architectsjournal.co.uk/pictures/2000x2000fit/9/4/2/1369942_London_Skyline.jpg', postcontent: "", upvotes: 9},
+        //    {title: 'There should be more articles like this...', subtitle: 'Yes I absolutely agree', submitter: 'Yusuf Osman', date: d2, image: 'https://wallpaperscraft.com/image/thailand_sea_ocean_boat_rock_95477_1920x1080.jpg',  postcontent: "", upvotes: 3},
+        //];
+
         $scope.addPost = function(){
             if(!$scope.postcontent || $scope.postcontent === '') { return; }
             $scope.posts.push({submitter: 'Yusuf Osman', date: Date(), postcontent: $scope.postcontent, upvotes: 0});
